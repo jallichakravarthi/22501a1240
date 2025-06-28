cf505feda1906ea159133c11fc32b92d2a05d7dd
JALLI CHAKRAVARTHI <jallichakravarthi@JALLIs-Mac-mini.local>
2025-06-28 12:50:04 +0530

    initial commit after adding frontend and backend

--- Frontend Test Submission/frontend/src/utils/logger.js
+++ Frontend Test Submission/frontend/src/utils/logger.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// Replace this with your actual token
-const ACCESS_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // Truncated for safety
+const ACCESS_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJqYWxsaWNoYWtyYXZhcnRoaUBnbWFpbC5jb20iLCJleHAiOjE3NTEwOTM3NjcsImlhdCI6MTc1MTA5Mjg2NywiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6ImMwYWU4ZmY1LTEzZWYtNGY4MS04ZDI1LTY0ZWRkMGZjYzA5NCIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImphbGxpIGNoYWtyYXZhcnRoaSIsInN1YiI6IjhhNzU2MGEyLTliZGItNGJkZC05NDVjLWE3OGFlZDI4M2U2MiJ9LCJlbWFpbCI6ImphbGxpY2hha3JhdmFydGhpQGdtYWlsLmNvbSIsIm5hbWUiOiJqYWxsaSBjaGFrcmF2YXJ0aGkiLCJyb2xsTm8iOiIyMjUwMWExMjQwIiwiYWNjZXNzQ29kZSI6ImVIV056dCIsImNsaWVudElEIjoiOGE3NTYwYTItOWJkYi00YmRkLTk0NWMtYTc4YWVkMjgzZTYyIiwiY2xpZW50U2VjcmV0IjoiTmZUa2JOSnpKbll5eXdZVyJ9.mZfjK1lVyPcuR3pfG00LVF3WJx7t9Za7E4B0I7-zUp0';
 
 const logEvent = async ({ level = 'info', message = '', stack = 'frontend', pkg = 'component' }) => {
   try {
